c72a496b2648d7b9de96967ef7ba01cc4bd3fadb
Shane Way <shane.c.way2@gmail.com>
2022-06-04 14:23:35 -0600

    This commit adds functionality to the new pallete button

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ var hexValues = document.querySelectorAll('.hex-values')
 var unlockImages = document.querySelectorAll('.image-button')
 var imageButtonContainer = document.getElementsByClassName('image-button-container')
 var colorContainer = document.querySelector('.colors-container')
+var newPaletteButton = document.querySelector(".new-palette");
 
 //EventListeners
 window.addEventListener('load', randomColor)
 colorContainer.addEventListener('click', function(event) {
   lockColor(event)
 })
+newPaletteButton.addEventListener('click', newPalette)
 
 var palette = new Palette()
 var color = new Color()
@@ -23,6 +25,8 @@ function randomColor() {
   }
 }
 
+
+
 function lockColor(event) {
   var hexId = event.target.id
   for (var i = 1; i <= unlockImages.length; i++) {
@@ -35,4 +39,11 @@ function lockColor(event) {
       unlockImages[(i - 1)].src = './assets/unlock.svg'
     }
   }
+}
+// We want to prevent a new palette from loading upon pressing the refresh button
+// When the "new Palette" button is clicked
+// We want to invoke a function that will generate a new set of colors to the palette container
+
+function newPalette() {
+  randomColor();
 }
