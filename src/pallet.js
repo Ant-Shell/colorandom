7ef7f7617620387b47c7dbe7773cf4651d11b333
Shane Way <shane.c.way2@gmail.com>
2022-06-05 12:38:34 -0600

    This commit has the getRandomColor function removed and added to the main.js file

--- src/pallet.js
+++ src/pallet.js
@@ -1,8 +1,3 @@
-function getRandomColor() {
-  var randNum = Math.floor(Math.random() * 16777215).toString(16);
-  return `#${randNum}`
-}
-
 class Palette {
   constructor() {
     this.id = Date.now();
