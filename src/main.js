fd6fcacc8a35b3717820cd42d15fc4fd8ec50a84
Shane Way <shane.c.way2@gmail.com>
2022-06-06 16:44:05 -0600

    This commit refactors the main.js file

--- src/main.js
+++ src/main.js
@@ -11,54 +11,80 @@ var savedPaletteWindow = document.querySelector(".palette-window");
 var closeSavedPaletteWindowButton = document.querySelector(".closeSavedPaletteWindow");
 var savePaletteSection = document.querySelector('.saved-palettes');
 
-
 //EventListeners
 window.addEventListener('load', newPalette)
 colorContainer.addEventListener('click', function(event) {
   toggleLockIcon(event)
 })
-newPaletteButton.addEventListener('click', lockRefresh)
+newPaletteButton.addEventListener('click', lockColors)
 savedPaletteButton.addEventListener('click', savePalette)
 accordionButton.addEventListener('click', displaySavedPaletteWindow);
 closeSavedPaletteWindowButton.addEventListener('click', closeSavedPaletteWindow)
 savePaletteSection.addEventListener('click', deleteSavedPalette)
 
-//var palette;
+// Global array
 var savedPalettes = [];
-
-function lockRefresh() {
-  for (var i = 0; i < palette.colors.length; i++) {
-    if (!palette.colors[i].locked) {
-      palette.colors[i] = new Color(getRandomColor())
-      }
-    }
-    for (var i = 0; i < palette.colors.length; i++) {
-        hexBox[i].style.backgroundColor = palette.colors[i].hexCode
-        hexValues[i].innerText = palette.colors[i].hexCode
-    }
-  }
 
 function newPalette() {
   palette = new Palette();
-  randomColor();
+  displayRandomColor();
 }
 
-function randomColor() {
+function getRandomHexCode() {
+  var randHex = ""
+  var string = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+  for (var i = 0; i <= 5; i++) {
+   randHex += string[Math.floor(Math.random() * 16)];
+  }
+  return `#${randHex}`
+}
+
+function displayRandomColor() {
   for (var i = 0; i < hexBox.length; i++) {
-    var color = getRandomColor()
+    var color = getRandomHexCode()
     hexBox[i].style.backgroundColor = color
     hexValues[i].innerText = color
     palette.generateColor(color)
   }
 }
 
-function getRandomColor() {
-  var randNum = ""
-  var string = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-  for (var i = 0; i <= 5; i++) {
-   randNum += string[Math.floor(Math.random() * 16)];
+function lockColors() {
+  for (var i = 0; i < palette.colors.length; i++) {
+    if (!palette.colors[i].locked) {
+      palette.colors[i] = new Color(getRandomHexCode())
+      }
+    }
+    for (var i = 0; i < palette.colors.length; i++) {
+        hexBox[i].style.backgroundColor = palette.colors[i].hexCode
+        hexValues[i].innerText = palette.colors[i].hexCode
   }
-  return `#${randNum}`
+}
+
+function savePalette() {
+    savedPalettes.push(palette)
+    displaySavedPalettes()
+    newPalette()
+}
+
+function displaySavedPalettes() {
+    savePaletteSection.innerHTML = ""
+    for (var i = 0; i < savedPalettes.length; i++) {
+      var newPaletteElement = document.createElement('figure')
+      newPaletteElement.classList.add('palettes-container')
+      for (var j = 0; j < savedPalettes[i].colors.length; j++) {
+        newPaletteElement.innerHTML += `<div class="hex-boxes" style="background-color: ${savedPalettes[i].colors[j].hexCode};height:25px;width:25px"></div>`
+      }
+      newPaletteElement.innerHTML += `<img class="hex-boxes" id=${i} style="height:25px;width:25px" src="./assets/bin.svg">`
+      savePaletteSection.appendChild(newPaletteElement)
+    }
+  }
+
+function displaySavedPaletteWindow() {
+    savedPaletteWindow.classList.remove('hidden');
+}
+
+function closeSavedPaletteWindow() {
+    savedPaletteWindow.classList.add('hidden');
 }
 
 function toggleLockIcon(event) {
@@ -74,35 +100,8 @@ function toggleLockIcon(event) {
   }
 }
 
-function savePalette() {
-  savedPalettes.push(palette)
-  displaySavedPalettes()
-  newPalette()
-}
-
 function deleteSavedPalette(event) {
   var id = event.target.id
   savedPalettes.splice(id, 1)
   displaySavedPalettes()
-}
-
-function displaySavedPalettes() {
-  savePaletteSection.innerHTML = ""
-  for (var i = 0; i < savedPalettes.length; i++) {
-    var newPaletteElement = document.createElement('figure')
-    newPaletteElement.classList.add('palettes-container')
-    for (var j = 0; j < savedPalettes[i].colors.length; j++) {
-      newPaletteElement.innerHTML += `<div class="hex-boxes" style="background-color: ${savedPalettes[i].colors[j].hexCode};height:25px;width:25px"></div>`
-    }
-    newPaletteElement.innerHTML += `<img class="hex-boxes" id=${i} style="height:25px;width:25px" src="./assets/bin.svg">`
-    savePaletteSection.appendChild(newPaletteElement)
-  }
-}
-
-function displaySavedPaletteWindow() {
-  savedPaletteWindow.classList.remove('hidden');
-}
-
-function closeSavedPaletteWindow() {
-  savedPaletteWindow.classList.add('hidden');
 }
